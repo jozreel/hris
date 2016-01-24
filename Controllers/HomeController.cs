3c2a6a761f5fb94fc528cb0cfbf283efb4c7066a
jozreel <jozreellaurent@gmail.com>
2016-01-24 08:13:20 -0400

    x

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
-
+using System.Web;
 namespace hris.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            
+           
             Response.AppendHeader("Access-Control-Allow-Origin", "*");
             return View();
         }
